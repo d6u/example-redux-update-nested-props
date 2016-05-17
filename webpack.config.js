d9b97a39dcc2473877dc30096a98b31106fa7d1b
Daiwei <daiweilu123@gmail.com>
2016-05-16 23:27:17 -0400

    Prod build React

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,22 @@ const config = {
       },
     ]
   },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    }),
+  ]
 };
 
 module.exports = config;
